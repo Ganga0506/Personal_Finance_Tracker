af59028f83c01c0212f7b2b85d41590f69b5eb9d
Ganga Nomika Mahesh <ganga.mahesh00@gmail.com>
2025-07-07 18:36:52 -0400

    Set up database and project structure
    * Project Structure
        * Created folders like app/, app/models/, and app/database/
        * Created empty main.py to implement logic later
    * Database Layout
        * Added database.py with SQLAlchemy setup using SQLite
        * Defined engine, sessionmaker, and Base
    * Transaction Model
        * Created models.py with a Transaction class:
            * Fields: id, name, amount, category, date
    * Database Initialization
        * Added init_db.py to initialize the database schema with Base.metadata.create_all(engine)
    * Organized module structure

---
+++ App/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Float, Date, Enum
+from database import Base
+import enum
+
+class CategoryEnum(enum.Enum):
+    FOOD = "Food"
+    TRANSPORT = "Transport"
+    FUN = "Fun"
+    UTILITIES = "Utilities"
+    HEALTH = "Health"
+    MISC = "MISC"
+
+class Transaction(Base): 
+    __tablename__ = 'transactions'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    amount = Column(Float, nullable=False)
+    category = Column(Enum(CategoryEnum), nullable=False)
+    date = Column(Date, nullable=False)
+
+    def __repr__(self):
+        return f"<Transaction(id={self.id}, name='{self.name}', amount={self.amount}, category='{self.category}', date={self.date})>"
